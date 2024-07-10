5d68cbf3a57949b58ca317fbd36c9e50629e48b5
namespace-Pt <61188463+namespace-Pt@users.noreply.github.com>
2024-07-10 18:36:04 +0800

    更新 args.py

--- src/args.py
+++ src/args.py
@@ -16,7 +16,7 @@ class ModelArgs:
         metadata={'help': 'Default path to save huggingface datasets.'}
     )
     data_root: str = field(
-        default="/data/ultragist", 
+        default="/data/long-llm", 
         metadata={'help': 'The base directory storing all data used for training and evaluation. If specified, make sure all train_data, eval_data, and corpus are path relative to data_root!'},
     )
     train_data: Optional[List[str]] = field(
